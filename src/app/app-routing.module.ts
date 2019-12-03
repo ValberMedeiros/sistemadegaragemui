9877fcdaa27bf7115bfce9334cf5e6ec7daebf6d
valberjunior <dev.junior.medeiros@gmail.com>
2019-12-03 18:57:55 -0300

    Initial Commit (Modulo de motoristas ja implementado)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { ListaMotoristasComponent } from './motoristas/lista-motoristas/lista-motoristas.component';
+import { CadastroMotoristasComponent } from './motoristas/cadastro-motoristas/cadastro-motoristas.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AtualizarMotoristasComponent} from "./motoristas/atualizar-motoristas/atualizar-motoristas.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'motoristas',  component: ListaMotoristasComponent},
+  { path: 'atualizacao/:idmotorista', component: AtualizarMotoristasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+import { SharedModule } from './shared/shared.module';
+import { MotoristasModule } from './motoristas/motoristas.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MotoristasModule,
+    SharedModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
